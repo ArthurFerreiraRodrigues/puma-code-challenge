accf27ea0f5e731bd62fb245659e657be710b78e
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2023-06-01 19:07:58 -0300

    Ssetting up enviroment variables

---
+++ backend/src/config/environment.js
@@ -0,0 +1,9 @@
+const maxProfilesQuantity = 5;
+const port = 3000;
+const githubApiUrl = 'https://api.github.com/users/';
+
+module.exports = {
+    maxProfilesQuantity,
+    port,
+    githubApiUrl,
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const app = require('./app');
-
-const port = 3000;
+const { port } = require('./config/environment');
 
 app.listen(port, () => {
     console.log(
         `> Backend PUMA Challenge is running on port ${port}. Access http://localhost:${port}`,
+    );
+    console.log(
+        '- See src/config/environment.js to change ports and set maximum number of profiles.\n',
     );
 });
