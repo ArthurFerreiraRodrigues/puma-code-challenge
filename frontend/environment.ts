2b334338f5528f740aa37f031d5003677efa511b
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2023-06-02 03:19:20 -0300

    (#8, #9) - Setting up env

---
+++ frontend/environment.ts
@@ -0,0 +1,8 @@
+// File that contains the environment variables for the application
+
+export const environment = {
+  production: false,
+  urlBackendApi: 'http://localhost:3000',
+
+  client_id: 'puma-challenge',
+};
