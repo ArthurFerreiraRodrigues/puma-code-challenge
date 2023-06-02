d46433a0fde417621bc91c8da9323d2defbdf564
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2023-06-02 00:18:26 -0300

    #3 #4 - List profiles and Star favorite user

--- backend/src/controller/profile.controller.js
+++ backend/src/controller/profile.controller.js
@@ -1,30 +1,53 @@
+const HttpStatus = require('http-status-codes');
 const { favoritedProfiles } = require('../model/Profile.class.model');
 const profileService = require('../service/profile.service');
 
 /**
  * Receives a Profile object on it's body and adds it to the list of favorited profiles
  */
-const addProfile = async (req, res, next) => {
-    try {
-        await profileService.addProfile(res.locals.retrievedProfile);
-        res.status(201).json(favoritedProfiles);
-    } catch (error) {
-        next(error);
-    }
+const addProfile = async (req, res) => {
+    await profileService.addProfile(res.locals.retrievedProfile);
+    res.status(201).json(res.locals.retrievedProfile);
 };
 
 /**
  * Lists all favorited profiles
  */
-const listProfiles = async (req, res, next) => {
+const listProfiles = async (req, res) => {
+    res.status(200).json(favoritedProfiles);
+};
+
+/**
+ * Toggles the starred status of a profile
+ */
+const toggleStar = async (req, res) => {
+    const { username } = req.params;
     try {
-        res.status(200).json(favoritedProfiles);
+        const starred = await profileService.toggleStar(
+            username,
+            res.locals.currentlyStarredProfile,
+        );
+        res.status(200).json(starred);
     } catch (error) {
-        next(error);
+        res.status(error.status).json({
+            error: `${error.status} ${HttpStatus.StatusCodes[error.status]
+                .toLowerCase()
+                .replace('_', ' ')}`,
+            message: error.message,
+        });
     }
+};
+
+/**
+ * Removes a profile from the list of favorited profiles
+ */
+const deleteProfile = async () => {
+
 };
 
 module.exports = {
     addProfile,
     listProfiles,
+    toggleStar,
+    deleteProfile,
 };
