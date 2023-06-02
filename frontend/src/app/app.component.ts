0249b920af1a0cda4f24f35a1ec0e64c90d07cd5
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2023-06-02 06:21:43 -0300

    (#11) - Erasing template + add mock

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+import { Profile } from './models/profile.model';
+import { HttpService } from './services/http.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'PUMA Challenge';
+
+  profiles: Profile[] = [];
+
+  constructor(private httpService: HttpService) {}
+
+  ngOnInit(): void {
+    this.httpService
+      .post<Profile[]>('users/', { username: 'ArthurFerreiraRodrigues' })
+      .subscribe((data) => {
+        console.log(data);
+      });
+
+    this.httpService.get<Profile[]>('users/').subscribe((data) => {
+      console.log(data);
+      this.profiles = data;
+    });
+  }
+
+  getProfile(i: number): Profile {
+    return this.profiles[i];
+  }
 }
