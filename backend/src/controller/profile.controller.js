0ffc34c1621e0703a630515e16ef975606395d07
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2023-06-01 21:36:57 -0300

    (#1) - Adding users to list of favorites

--- backend/src/controller/profile.controller.js
+++ backend/src/controller/profile.controller.js
@@ -1,22 +1,13 @@
-const { Profile } = require('../model/Profile.class.model').Profile;
 const { favoritedProfiles } = require('../model/Profile.class.model');
 const profileService = require('../service/profile.service');
 
-/** 
+/**
  * Receives a Profile object on it's body and adds it to the list of favorited profiles
-*/
+ */
 const addProfile = async (req, res, next) => {
     try {
-        let { username, name, avatar, url } = req.body;
-
-        await profileService.addProfile({
-            username,
-            name,
-            avatar,
-            url,
-        });
-
-        res.status(200).json(favoritedProfiles);
+        await profileService.addProfile(res.locals.retrievedProfile);
+        res.status(201).json(favoritedProfiles);
     } catch (error) {
         next(error);
     }
