f8a6fed2820991c5b5957c1ea5d7e394f157b6d4
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2023-06-01 21:31:50 -0300

    (#6) - Retrieving data from github's api

--- backend/src/service/profile.service.js
+++ backend/src/service/profile.service.js
@@ -1,4 +1,37 @@
+const fetch = require('node-fetch');
 const { Profile, favoritedProfiles } = require('../model/Profile.class.model');
+
+const { githubApiUrl } = require('../config/environment');
+
+/**
+ * Retrieve a github profile from the github api
+ *
+ * @param {string} username
+ * @returns {Profile}
+ * @throws {Error} if the username is invalid
+ * @throws {Error} if the github api request fails
+ */
+const getGithubProfile = async (username) => {
+    const githubRes = await fetch(`${githubApiUrl}${username}`);
+    if (githubRes.status === 404) {
+        throw {
+            status: 404,
+            message: 'Invalid username',
+        };
+    }
+
+    if (githubRes.status !== 200) {
+        throw new Error('Github API request failed');
+    }
+
+    const githubProfile = await githubRes.json();
+    return Profile.create({
+        username: githubProfile.login,
+        name: githubProfile.name,
+        avatar: githubProfile.avatar_url,
+        url: githubProfile.html_url,
+    });
+};
 
 /**
  * @param {Profile} profileBody
@@ -10,5 +43,6 @@ const addProfile = async (profileBody) => {
 };
 
 module.exports = {
+    getGithubProfile,
     addProfile,
 };
